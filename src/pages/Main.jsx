ca36de904bf2ca64e0cd93fa969145e894efbd20
argieldev <argielducut8@gmail.com>
2026-06-05 22:07:22 +0800

    First Commit and Base of Sidebar

---
+++ src/pages/Main.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "../components/main/Sidebar";
+
+function Main() {
+    return(
+        <main className="flex h-screen">
+            <Sidebar />
+        </main>
+    );
+}
+
+export default Main;
